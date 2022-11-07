ac5e1b352f4a908cb9944892ebc79ed048e3199f
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-11-07 20:24:10 +0000

    Change names again and split out thirdparty

--- src/OpenLoco/src/Core/FileSystem.hpp
+++ src/OpenLoco/src/Core/FileSystem.hpp
@@ -24,7 +24,7 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 #else
-#include <Thirdparty/filesystem.hpp>
+#include <ThirdParty/Stdx/filesystem.hpp>
 namespace fs = ghc::filesystem;
 #endif
 
--- src/OpenLoco/src/Core/Variant.hpp
+++ src/OpenLoco/src/Core/Variant.hpp
@@ -14,7 +14,7 @@ namespace stdx
 }
 #else
 
-#include <Thirdparty/variant.hpp>
+#include <ThirdParty/Stdx/variant.hpp>
 namespace stdx
 {
     using mpark::variant;
